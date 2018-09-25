a49bd8b3b3e8e1851768d8bbe6f768d55252cf94
KentaHikosaka <khikosaka@netprotections.co.jp>
2018-09-25 23:01:49 +0900

    commit

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
   def top
+    Post.create(content: "test")
   end
   def about
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   get 'posts/index' => "posts#index"
   get "posts/:id" => "posts#show"
- 
+
   get '/' => "home#top"
   get "about" =>"home#about"
-
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
